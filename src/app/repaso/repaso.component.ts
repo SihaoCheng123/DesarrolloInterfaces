4ab751dc17a54ad8f8b9c0dec74833d34a0753cd
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2024-10-30 11:29:20 +0100

    repaso

---
+++ src/app/repaso/repaso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repaso',
+  templateUrl: './repaso.component.html',
+  styleUrl: './repaso.component.scss'
+})
+export class RepasoComponent {
+
+}
